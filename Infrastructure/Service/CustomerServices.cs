65148330c599cf0b4b7eaf92635b89ad42e73eaf
Rasil-MuzaFFarov <rasul_rasul1945@mail.ru>
2023-06-18 17:48:06 +0500

    first commit

---
+++ Infrastructure/Service/CustomerServices.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Dtos.Employee;
+using Domain.Entities;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Services;
+
+public class CustomerServices
+{
+    private readonly DataContext _context;
+    
+    public CustomerServices(DataContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<List<CustomerDto>> GetCustomers()
+    {
+        return await _context.Customers.Select(e=> new CustomerDto()
+        {
+            Id = e.Id,
+            FirstName = e.FirstName,
+            LastName = e.LastName,
+            Address = e.Address,
+            Phone = e.Phone,
+            Email = e.Email
+            
+        }).ToListAsync();
+    }
+    
+    public async Task<Customer> GetCustomerById(int id)
+    {
+        return await _context.Customers.FindAsync(id);
+    }
+    
+    public async Task<Customer> AddCustomer(Customer Customer)
+    {
+        await _context.Customers.AddAsync(Customer); 
+        await _context.SaveChangesAsync();
+        return Customer;
+        
+    }
+
+    public async Task<Customer> AddCustomerById(int id)
+    {
+        
+        return await _context.Customers.FindAsync(id);
+    }
+}
---
+++ WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿//
+// using Domain.Dtos.Employee;
+// using Infrastructure.Services;
+// using Microsoft.AspNetCore.Mvc;
+//
+// namespace WebApi.Controllers;
+//
+// [ApiController]
+// [Route("[controller]")]
+// public class EmployeeController :ControllerBase
+// {
+//     private readonly EmployeeService _employeeService;
+//
+//     public EmployeeController(EmployeeService employeeService)
+//     {
+//         _employeeService = employeeService;
+//     }
+//
+//     [HttpGet("get-employees")]
+//     public async Task<List<EmployeeDto>> GetEmployee()
+//     {
+//         return await _employeeService.GetEmployees();
+//     }
+//     
+// }
